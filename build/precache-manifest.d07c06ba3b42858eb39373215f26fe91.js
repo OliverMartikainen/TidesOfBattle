addd0d31dd625a947b636834369571fbe46d7a2c
olivermart <48790588+olivermart@users.noreply.github.com>
2019-07-07 02:37:57 +0300

    initial

---
+++ build/precache-manifest.d07c06ba3b42858eb39373215f26fe91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e778632c1c8316008be346bc9b61fcd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248630690fcc05ca0307",
+    "url": "/static/css/main.59d3deca.chunk.css"
+  },
+  {
+    "revision": "d4a3b392a4f970ee7e26",
+    "url": "/static/js/2.3bcf33f2.chunk.js"
+  },
+  {
+    "revision": "248630690fcc05ca0307",
+    "url": "/static/js/main.ac983716.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
