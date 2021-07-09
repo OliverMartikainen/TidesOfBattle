98fe51e77ac7eb1db727b9e237815d6e48472a61
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2021-07-10 02:24:37 +0300

    restart

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es6': true,
+		'node': true,
+		'jest': true
+	},
+	'extends': 'eslint:recommended',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2020
+	},
+	'rules': {
+		'indent': [
+			'error',
+			4,
+			{ 'SwitchCase': 1 }
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
--- index.js
+++ index.js
@@ -1,72 +1,40 @@
 const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const fs = require('fs')
-
-app.use(bodyParser.json())
 const cors = require('cors')
+const compression = require('compression')
+
+const app = express()
+
+
+const shouldCompress = (req, res) => {
+    if (req.headers['x-no-compression']) {
+        return false
+    }
+    /* with this the SSE route of /eventSubscriptions will not use gzip compression.
+  Frontend doesnt understand gzip EventSource messages atm, no real reason to implement it either. */
+    if (req.url === '/eventSubscription') return false
+
+    return compression.filter(req, res)
+}
+
+
+app.use(express.json())
 app.use(cors())
-var publicDir = require('path').join(__dirname,'/public');
-app.use(express.static(publicDir));
+app.use(compression({
+    filter: shouldCompress,
+}))
 
 app.use(express.static('build'))
 
-const cards = [
-  {
-    id: "zero",
-    path: "/resources/zero.jpg"
-  },
-  {
-    id: "zero_skull",
-    path: `/resources/zero_skull.jpg`
-  },
-  {
-    id: "one_sword",
-    path: `/resources/one_sword.jpg`
-  },
-  {
-    id: "one_tower",
-    path: `/resources/one_tower.jpg`
-  },
-  {
-    id: "two",
-    path: `/resources/two.jpg`
-  },
-  {
-    id: "three",
-    path: `/resources/three.jpg`
-  }
-]
-
 
 app.get('/', (req, response) => {
-  response.send("shits hit shit")
+    response.send('shits hit shit')
 })
 
-app.get('/cards', (req, response) => {
-  if (cards) {
-    response.json(cards)
-  }
-  else {
-    response.status(404).end()
-  }
-})
 
-app.get('/cards/:id', (req, response) => {
-  console.log("shitshithsit")
 
-  if (cards) {
-    const id = req.params.id
-    const card = cards.find(card => card.id === id)
-    response.json(card)
-  }
-  else {
-    response.status(404).end()
-  }
-})
 
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
