61e5071568999ed03d99931ad2db7f28f8fff7f9
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2022-08-03 15:07:02 +0300

    Initial addition of spring server

---
+++ back-javaSpring/src/main/java/com/tides/backJava/BackJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tides.backJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackJavaSpringApplication.class, args);
+	}
+
+}
---
+++ back-javaSpring/src/test/java/com/tides/backJava/BackJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tides.backJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
