4c00443d9759297e4b28df27166f03a96e520b39
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2022-08-03 16:14:23 +0300

    Update JavaServerSpringApplication.java

---
+++ back-javaSpring/src/main/java/com/javaserver/JavaServerSpringApplication.java
@@ -0,0 +1,22 @@
+package com.javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaServerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerSpringApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name, @RequestParam(value = "name2", defaultValue = "World2") String name2) {
+		return String.format("Helloff %s! %s2aa", name, name2);
+	}
+}
