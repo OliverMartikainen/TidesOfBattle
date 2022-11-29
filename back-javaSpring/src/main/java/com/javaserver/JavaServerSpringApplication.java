08284410f3f18cde78f975318d0eb00e33b1e4fa
OliverMartikainen <48790588+OliverMartikainen@users.noreply.github.com>
2022-11-29 14:45:51 +0200

    Update JavaServerSpringApplication.java

--- back-javaSpring/src/main/java/com/javaserver/JavaServerSpringApplication.java
+++ back-javaSpring/src/main/java/com/javaserver/JavaServerSpringApplication.java
@@ -1,7 +1,12 @@
 package com.javaserver;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +19,18 @@ public class JavaServerSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaServerSpringApplication.class, args);
+		
+		ResourceLoader resourceLoader = new DefaultResourceLoader();
+		Resource resource = resourceLoader.getResource("classpath:static/index.html");
+		try {
+			System.out.println(resource.exists() + " " + resource.toString());
+			System.out.println(resource.getURL());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
 	}
 
 
